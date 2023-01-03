fa748fd1311227e03007a5946bb9bf13eaec1db8
Mahdi <billgates1372@gmail.com>
2023-01-04 02:20:25 +0330

    Add project files.

---
+++ DontUseAutoMapper/ProductDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DontUseAutoMapper
+{
+    public class ProductDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+
+        public static explicit operator ProductDto(Product product)
+        {
+            return new ProductDto
+            {
+                Name = product.ProductName,
+                Description = product.ProductDescription,
+                Price = product.Price + product.Price * 100 / product.VatPercentage
+            };
+        }
+    }
+}
